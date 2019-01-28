d10e6018f2365b3893cfae3450307a22d83f3a02
Darong Qi <qi.darong@gmail.com>
2019-01-28 16:19:40 -0600

    Initial Commit

---
+++ tipster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tipster
+//
+//  Created by Darong Qi on 1/28/19.
+//  Copyright © 2019 Darong's Projects. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
